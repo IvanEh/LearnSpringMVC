dd4d37f8f520886a0d452f2e99d7cd951273e394
ivaneh <ivanehreshi@gmail.com>
2016-07-05 17:51:43 +0300

    Linux tomcat instance

---
+++ tomcat/bin/shutdown.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CATALINA_BASE="/home/ivaneh/Workspace/java/spring/LearnMvc/tomcat"
+/usr/share/tomcat8/bin/shutdown.sh
+echo "Tomcat stopped"
---
+++ tomcat/bin/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CATALINA_BASE="/home/ivaneh/Workspace/java/spring/LearnMvc/tomcat"
+/usr/share/tomcat8/bin/startup.sh
+echo "Tomcat started"
